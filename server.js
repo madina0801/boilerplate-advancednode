f101ce81880ff1fd28bc071a8c6454ecc4b52b0f
Madina <madinatussupova0820@gmail.com>
2023-11-30 15:46:30 +0600

    remove the first / route

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use(session({
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
-app.route('/').get((req, res) => {
-  res.render('index', { title: 'Hello', message: 'Please log in' })
-});
+// app.route('/').get((req, res) => {
+//   res.render('index', { title: 'Hello', message: 'Please log in' })
+// });
 
 const PORT = process.env.PORT || 3000;
 
