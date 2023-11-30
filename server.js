49b556f5dfe0853364c064ab269a19a7759ffc30
Madina <madinatussupova0820@gmail.com>
2023-11-30 13:48:19 +0600

    calling done in deserialization with doc

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ myDB(async client => {
   
   passport.deserializeUser((id, done) => {
     myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, null);
+      done(null, doc);
     });
   });
 }).catch(e => {
