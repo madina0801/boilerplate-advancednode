a500607b70df0d7a8cacbe6a7d6794a18f14b1eb
Madina <madinatussupova0820@gmail.com>
2023-12-01 09:14:09 +0600

    put a profile together

--- server.js
+++ server.js
@@ -58,7 +58,9 @@ myDB(async client => {
     })
 
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
-    res.render('profile')
+    res.render('profile', {
+      username: req.user.username
+    })
   })
 
   passport.serializeUser((user, done) => {
