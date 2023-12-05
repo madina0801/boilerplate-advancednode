b8254f0527e24ec45849accd5568d9c8c6db5bab
Madina <madinatussupova0820@gmail.com>
2023-12-05 14:42:06 +0600

    client handle and display chat messages

--- public/client.js
+++ public/client.js
@@ -13,6 +13,12 @@ $(document).ready(function () {
       (data.connected ? ' has joined the chat.' : ' has left the chat.');
     $('#messages').append($('<li>').html('<b>' + message + '</b>'));
   });
+
+  socket.on('chat message', data => {
+    console.log('socket.on 1')
+    $('#messages').append($('<li>').text(`${data.username}: ${data.message}`));
+  });
+  
   // Form submittion with new message in field with id 'm'
   $('form').submit(function () {
     var messageToSend = $('#m').val();
